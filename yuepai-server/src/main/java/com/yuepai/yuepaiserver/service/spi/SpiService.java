f803f00a608a68d687a32f4937b66fccd7beda78
吴翰文 <p78o233@126.com>
2019-08-02 15:06:48 +0800

    dubbo spi

---
+++ yuepai-server/src/main/java/com/yuepai/yuepaiserver/service/spi/SpiService.java
@@ -0,0 +1,10 @@
+package com.yuepai.yuepaiserver.service.spi;/*
+ * @author p78o2
+ * @date 2019/8/2
+ */
+
+import com.alibaba.fastjson.JSONObject;
+
+public interface SpiService {
+    public JSONObject testSpi();
+}
---
+++ yuepai-server/src/main/java/com/yuepai/yuepaiserver/service/spi/impl/SpiServiceSecond.java
@@ -0,0 +1,18 @@
+package com.yuepai.yuepaiserver.service.spi.impl;/*
+ * @author p78o2
+ * @date 2019/8/2
+ */
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.alibaba.fastjson.JSONObject;
+import com.yuepai.yuepaiserver.service.spi.SpiService;
+
+@Service(group = "group2",version = "1.0.0",timeout = 10000)
+public class SpiServiceSecond implements SpiService{
+    @Override
+    public JSONObject testSpi() {
+        JSONObject jo = new JSONObject();
+        jo.put("data","第二个实现类");
+        return jo;
+    }
+}
